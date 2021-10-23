ddce976afd094bc1fa7716cc26d0d09cbfb82322
Anza1 <93040199+Anza1@users.noreply.github.com>
2021-10-23 17:36:11 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+var budget;
+
+var NameMagazine; 
+
+var shopGoods = [ , , ,];
+
+var employers = {
+    
+}
+
+var mainList = {
+    budget,
+    NameMagazine,
+    shopGoods,
+}
+
+budget = prompt ('Budget?');
+NameMagazine = prompt ( 'Name Magazine?');
+shopGoods[0] = prompt ('What sell?');
+shopGoods[1] = prompt ('What sell?');
+shopGoods[2] = prompt ('What sell?');
+
+alert(`Budget for one day = ${budget / 30}`)
+
